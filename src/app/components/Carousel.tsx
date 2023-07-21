c770aab3597a25f51bd9dfd4c2e52acca7ceef59
Hikariyen881230 <hikariyen881230@gmail.com>
2023-07-21 16:21:38 +0800

    [ add ]新增rwd寬度

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -4,14 +4,16 @@ import { Card } from './Card'
 
 export const Carousel = (props: carouselType) => {
   return (
-    <div className="wrapper w-full h-screen flex justify-center items-center">
-      <div className="container py-4">
+    <>
+      <div className="w-full md:w-3/4 py-4">
         <div className="font-GothamBold font-bold text-center">
-          <h2 className="text-5xl">Best Seller Gaming PC</h2>
-          <h4 className="text-3xl">Prebuilt & Custom</h4>
+          <h2 className="text-2xl md:text-4xl xl:text-5xl">
+            Best Seller Gaming PC
+          </h2>
+          <h4 className="text-xl md:text-2xl xl:text-3xl">Prebuilt & Custom</h4>
         </div>
         <Card />
       </div>
-    </div>
+    </>
   )
 }
